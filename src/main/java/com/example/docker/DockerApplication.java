2a4c55f2737be001fee726f955a82a14ec452aaa
up1 <somkiat.p@gmail.com>
2019-09-16 16:18:38 +0700

    Working with AOP

--- src/main/java/com/example/docker/DockerApplication.java
+++ src/main/java/com/example/docker/DockerApplication.java
@@ -2,8 +2,10 @@ package com.example.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class DockerApplication {
 
 	public static void main(String[] args) {
