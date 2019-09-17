0ba42983bd41a64d125969625c676a1a55db4079
up1 <somkiat.p@gmail.com>
2019-09-17 09:03:33 +0700

    Call service 2

--- src/main/java/com/example/docker/hello/HelloController.java
+++ src/main/java/com/example/docker/hello/HelloController.java
@@ -1,8 +1,10 @@
 package com.example.docker.hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
@@ -13,6 +15,21 @@ public class HelloController {
     @GetMapping("/ping")
     public String ping() {
         return helloService.getResult();
+    }
+
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/call")
+    public String callAnotherService() {
+        String result = restTemplate.getForObject("http://localhost:8082/target", String.class);
+        return result;
     }
 
 }
