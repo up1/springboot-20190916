733d8cd742bd37c8043da4236999cabbdf6d8884
up1 <somkiat.p@gmail.com>
2019-09-17 11:01:28 +0700

    Working with metric

--- src/main/java/com/example/docker/hello/HelloController.java
+++ src/main/java/com/example/docker/hello/HelloController.java
@@ -1,9 +1,12 @@
 package com.example.docker.hello;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +19,21 @@ public class HelloController {
     @GetMapping("/ping")
     public String ping() {
         return helloService.getResult();
+    }
+
+    // Custom my metric
+
+    @Autowired
+    private MeterRegistry meterRegistry;
+
+    @GetMapping("/hi/{name}")
+    public String hi(@PathVariable String name) {
+
+        // Metric name = tracking.user
+        Counter counter = meterRegistry.counter("tracking.user", "username", name);
+        counter.increment();
+
+        return "Hi, " + name;
     }
 
 
